4854ce29f5f37fc708b96a5ad067b4c8cbcf5d7c
Steve Gordon <sgordon@hotmail.co.uk>
2021-01-19 16:12:45 +0000

    Initial commit

---
+++ src/JsonBenchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JsonBenchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
